b8135c0d818bd0dcb2fc073962762a989e4ac5da
aJLax <chernykh-2004@yandex.ru>
2023-11-18 00:11:55 +0300

    +full index main page

---
+++ imageMovementRight.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let image = document.querySelector('.weather-image img');
+    
+    // Изначально устанавливаем translateY за пределами видимой области
+    image.style.transform = 'translateY(-100vh)';
+
+    // Задаем анимацию при появлении элемента
+    image.style.transition = 'transform 1s ease-in-out';
+
+    // Задаем новое значение transform, чтобы выезжать на экран
+    setTimeout(function() {
+        image.style.transform = 'translateY(0)';
+    }, 100);
+});
+
+
+
